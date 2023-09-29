244fe77faff9818e71f1244506e1d1c0d77961c7
Javier Collao <javier.collao@usach.cl>
2023-09-29 15:36:27 -0300

    Update entities

--- src/main/java/spring/topeducation/repository/ColegioRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.topeducation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import spring.topeducation.entities.ColegioEntity;
-
-@Repository
-public interface ColegioRepository extends JpaRepository<ColegioEntity, Long> {
-}
